5391cc534e906316823574760b55a663efeeff52
Shania Joseph <shania.joseph@aol.com>
2022-09-22 17:43:47 -0400

    implemented toggle button

--- script.js
+++ script.js
@@ -25,5 +25,54 @@ function toggleColor() {
 
 
 function toggleColorLALP() {
-	
+	let testElement = document.getElementById("topH1").style.color;
+	if (testElement === 'white') {
+		document.getElementById("topH1").style.color = "#663399";
+		document.getElementById("bodyElement").style.backgroundColor ="#E6E6FA";
+		document.getElementById("linkToHmPg").style.color = "#663399";
+		document.getElementById("firstP").style.color = "#663399";
+		document.getElementById("figCap1").style.color = "#9370DB";
+		document.getElementById("figCap2").style.color = "#9370DB";
+		document.getElementById("figCap3").style.color = "#9370DB";
+		document.getElementById("table").style.color = "#663399";
+		document.getElementById("tblElements1").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements2").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements3").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements4").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements5").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements6").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements7").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements8").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements9").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements10").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements11").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements12").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements13").style.borderColor = "#D8BFD8";
+		document.getElementById("tblElements14").style.borderColor = "#D8BFD8";
+		document.getElementById("h3Text").style.color = "#663399";
+	} else {
+		document.getElementById("topH1").style.color = 'white';
+		document.getElementById("bodyElement").style.backgroundColor = '#4169E1';
+		document.getElementById("linkToHmPg").style.color = "#6495ED";
+		document.getElementById("firstP").style.color = "white";
+		document.getElementById("figCap1").style.color = "white";
+		document.getElementById("figCap2").style.color = "white";
+		document.getElementById("figCap3").style.color = "white";
+		document.getElementById("table").style.color = "#6495ED";
+		document.getElementById("tblElements1").style.borderColor = "#6495ED";
+		document.getElementById("tblElements2").style.borderColor = "#6495ED";
+		document.getElementById("tblElements3").style.borderColor = "#6495ED";
+		document.getElementById("tblElements4").style.borderColor = "#6495ED";
+		document.getElementById("tblElements5").style.borderColor = "#6495ED";
+		document.getElementById("tblElements6").style.borderColor = "#6495ED";
+		document.getElementById("tblElements7").style.borderColor = "#6495ED";
+		document.getElementById("tblElements8").style.borderColor = "#6495ED";
+		document.getElementById("tblElements9").style.borderColor = "#6495ED";
+		document.getElementById("tblElements10").style.borderColor = "#6495ED";
+		document.getElementById("tblElements11").style.borderColor = "#6495ED";
+		document.getElementById("tblElements12").style.borderColor = "#6495ED";
+		document.getElementById("tblElements13").style.borderColor = "#6495ED";
+		document.getElementById("tblElements14").style.borderColor = "#6495ED";
+		document.getElementById("h3Text").style.color = 'white';
+	}
 }
